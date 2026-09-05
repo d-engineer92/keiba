507df572626c0f49ecebe47ad03ab1a9d4edef95
Daisuke Kurosawa <100755738+d-engineer92@users.noreply.github.com>
2026-09-06 06:59:45 +0900

    Keep database timezone change focused

--- config/database.php
+++ config/database.php
@@ -122,9 +122,9 @@ return [
     | Migration Repository Table
     |--------------------------------------------------------------------------
     |
-    | This table keeps track of all of the database migrations that have already run.
-    | Using this information, we can determine which of the migrations on disk
-    | haven't actually been run on the database.
+    | This table keeps track of all the migrations that have already run for
+    | your application. Using this information, we can determine which of
+    | the migrations on disk haven't actually been run on the database.
     |
     */
 
