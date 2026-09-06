375597acacf02b1d64f3244bc10164bf2c1b3d4b
Daisuke Kurosawa <100755738+d-engineer92@users.noreply.github.com>
2026-09-06 12:47:39 +0900

    refactor: version KD3 speed reference model

--- config/kd3.php
+++ config/kd3.php
@@ -9,9 +9,10 @@ return [
     'timeout' => 60,
     'storage_disk' => env('KD3_STORAGE_DISK', 'local'),
     'lzh_command' => env('KD3_LZH_COMMAND', 'lha'),
-    'parser_version' => '1.0.1',
-    'importer_version' => '1.0.0',
+    'parser_version' => '1.1.0',
+    'importer_version' => '1.1.0',
     'speed_calculation_version' => '1.0.0',
+    'speed_reference_version' => '1.0.0',
     'spec_version' => 'canonical-2025',
     'artifacts' => [
         'hb' => ['entry_pattern' => '/^kd3_hb{ymd2}\.lzh$/i'],
